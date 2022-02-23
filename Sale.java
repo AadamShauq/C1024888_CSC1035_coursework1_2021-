846ef10a1ae575b7c06d7ac82ee0cdcd215059fe
AadamShauq <A.A.Shauq1@newcastle.ac.uk>
2022-02-23 12:22:29 +0000

    Sale constructors created

---
+++ Sale.java
@@ -0,0 +1,13 @@
+public class Sale {
+    Integer HouseNumber;
+    String Postcode;
+    Integer year;
+    double Price;
+
+    public Sale(Integer HouseNumber,String Postcode,Integer year, double Price){
+        this.HouseNumber = HouseNumber;
+        this.Postcode = Postcode;
+        this.year = year;
+        this.Price = Price;
+    }
+}
