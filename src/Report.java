b92e77ffecdb10a21c9b2f7a0dc2756f93ae8c45
AadamShauq <A.A.Shauq1@newcastle.ac.uk>
2022-02-25 03:08:31 +0000

    BranchHighVal and BranchHighSale method completed

---
+++ src/Report.java
@@ -0,0 +1,32 @@
+public class Report {
+
+    public Branch name;
+
+    public double BranchHighVal(int targetYear) {
+        double total = 0;
+        int count = 0;
+        double branchVal = 0;
+
+        for (Sale sale : branchSales) {
+            if (sale.year == targetYear) {
+                total += sale.price;
+                count += 1;
+                branchVal = branch;
+            }
+        }
+        return branchVal;
+    }
+
+    public Sale HighSale() {
+        double highest=0;
+        Sale curSale=null;
+        for (Sale sale : branchSales) {
+            if (sale.price>highest) {
+                highest = sale.price;
+                curSale = sale;
+            }
+        }
+        return curSale;
+    }
+
+    public
