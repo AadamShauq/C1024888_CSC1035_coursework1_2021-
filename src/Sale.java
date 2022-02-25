6eb419d6db0b9052af6421ff6d8b93fcbb9ab6ed
AadamShauq <A.A.Shauq1@newcastle.ac.uk>
2022-02-25 05:24:24 +0000

    Javadoc complete on Sale class

--- src/Sale.java
+++ src/Sale.java
@@ -1,3 +1,7 @@
+/**
+ * This is a class representing a Sale object. It has a
+ * method making the object of a sale.
+ */
 public class Sale {
     public Integer houseNumber;
     public String postcode;
@@ -5,6 +9,15 @@ public class Sale {
     public Integer year;
     public double price;
 
+    /**
+     * This is a constructor that lines up all the parameter values
+     * with the field variables
+      * @param houseNumber represents the house number of the sale
+     * @param postcode represents the post code of the sale
+     * @param month represents the month of the sale
+     * @param year represents the year of the sale
+     * @param price represents the price of the sale
+     */
     public Sale(Integer houseNumber,String postcode,Integer month,Integer year, double price){
         this.houseNumber = houseNumber;
         this.postcode = postcode;
